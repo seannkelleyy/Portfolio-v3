d6a033c8e3c28e4c186b9151769a6606adddc01b
seankelley <kelley.1102@buckeyemail.osu.edu>
2025-06-11 18:42:16 -0400

    Updates resume photo and footer.

--- src/components/pageSections/contact/ContactFooter.tsx
+++ src/components/pageSections/contact/ContactFooter.tsx
@@ -2,6 +2,7 @@ export const ContactFooter = () => {
 	return (
 		<div className='text-center mb-4'>
 			<p>This website was built by Sean Kelley, 2024</p>
+			<p>Last updated 11 June 2025</p>
 			<p>Columbus, Ohio</p>
 			<p>Phone: (419) 980-6808</p>
 			<p>
